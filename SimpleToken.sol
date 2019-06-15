5c285669a22f54386509354752d29d2d4925f221
FANCLUBCOIN <49503556+FANCLUBCOIN@users.noreply.github.com>
2019-06-16 00:35:45 +0900

    Update SimpleToken.sol

--- SimpleToken.sol
+++ SimpleToken.sol
@@ -1,7 +1,8 @@
 pragma solidity ^0.5.0;
 
-import "../token/ERC20/ERC20.sol";
-import "../token/ERC20/ERC20Detailed.sol";
+import "./token/ERC20/ERC20.sol";
+import "./token/ERC20/ERC20Detailed.sol";
+import "./token/ERC20/ERC20Burnable.sol";
 
 /**
  * @title SimpleToken
@@ -10,11 +11,13 @@ import "../token/ERC20/ERC20Detailed.sol";
  * `ERC20` functions.
  */
 contract SimpleToken is ERC20, ERC20Detailed {
+    uint8 public constant DECIMALS = 18;
+    uint256 public constant INITIAL_SUPPLY = 10000 * (10 ** uint256(DECIMALS));
 
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-    constructor () public ERC20Detailed("SimpleToken", "SIM", 18) {
-        _mint(msg.sender, 10000 * (10 ** uint256(decimals())));
+    constructor () public ERC20Detailed("SimpleToken", "SIM", DECIMALS) {
+        _mint(msg.sender, INITIAL_SUPPLY);
     }
 }
